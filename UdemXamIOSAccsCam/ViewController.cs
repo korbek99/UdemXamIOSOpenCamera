93edede3154c407daea818682f3432ebb142f68b
korbek100 <korbek100@gmail.com>
2018-05-03 22:27:30 -0300

    first commit

---
+++ UdemXamIOSAccsCam/ViewController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using UIKit;
+
+namespace UdemXamIOSAccsCam
+{
+    public partial class ViewController : UIViewController
+    {
+
+        UIImagePickerController SeleccionImagen;
+        UIImage Fotografia;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            SeleccionImagen = new UIImagePickerController();
+            SeleccionImagen.FinishedPickingMedia += FinalizandoSeleccion;
+            SeleccionImagen.Canceled += CancelarSeleccion;
+            BtnCargar.TouchUpInside += delegate {
+                SeleccionImagen.SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+                PresentViewController(SeleccionImagen,true,null);
+            };
+
+            BtnCargarCamara.TouchUpInside += delegate {
+                SeleccionImagen.SourceType = UIImagePickerControllerSourceType.Camera;
+                PresentViewController(SeleccionImagen, true, null);
+            };
+        }
+
+        private void CancelarSeleccion(object sender , EventArgs e )
+        {
+            SeleccionImagen.DismissViewControllerAsync(true);
+
+        }
+
+        private void FinalizandoSeleccion(object sender, UIImagePickerMediaPickedEventArgs e)
+        {
+            Fotografia = e.Info[UIImagePickerController.OriginalImage] as UIImage;
+            Imagen.Image = Fotografia;
+            SeleccionImagen.DismissViewControllerAsync(true);
+
+
+        }
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ UdemXamIOSAccsCam/ViewController.designer.cs
@@ -0,0 +1,42 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace UdemXamIOSAccsCam
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UIButton BtnCargar { get; set; }
+
+		[Outlet]
+		UIKit.UIButton BtnCargarCamara { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView Imagen { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (BtnCargar != null) {
+				BtnCargar.Dispose ();
+				BtnCargar = null;
+			}
+
+			if (Imagen != null) {
+				Imagen.Dispose ();
+				Imagen = null;
+			}
+
+			if (BtnCargarCamara != null) {
+				BtnCargarCamara.Dispose ();
+				BtnCargarCamara = null;
+			}
+		}
+	}
+}
